c9cf0fb8d1b12a657f188f14c87fb3796c979274
vinChuli <37913511+wechulimaven@users.noreply.github.com>
2023-09-18 08:49:41 +0300

    removing super user

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,7 +3,7 @@
 set -e  # Configure shell so that if one command fails, it exits
 
 python manage.py migrate
-python manage.py create_super_user
+# python manage.py create_super_user
 # flake8
 
 gunicorn file_server.wsgi:application --bind 0.0.0.0:8000 --timeout 120 --workers 3
